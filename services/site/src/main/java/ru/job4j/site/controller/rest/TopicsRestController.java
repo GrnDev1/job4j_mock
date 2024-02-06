cff785358cf79a9095a1272b38cbde1a92bc47cb
Roman <grndev@mail.ru>
2024-02-06 16:08:05 +0300

    Initial commit

---
+++ services/site/src/main/java/ru/job4j/site/controller/rest/TopicsRestController.java
@@ -0,0 +1,28 @@
+package ru.job4j.site.controller.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.job4j.site.dto.TopicDTO;
+import ru.job4j.site.service.TopicsService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/topics_rest")
+@AllArgsConstructor
+public class TopicsRestController {
+
+    private final TopicsService topicsService;
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<List<TopicDTO>> getTopics(@PathVariable int categoryId)
+            throws JsonProcessingException {
+        return new ResponseEntity<>(topicsService.getByCategory(categoryId), HttpStatus.OK);
+    }
+}
